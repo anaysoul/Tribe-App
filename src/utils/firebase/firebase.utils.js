ea8181f298d0161d4258ca89924b3083095161ac
anaysoul <soulyana_lakew@outlook.com>
2022-03-20 07:25:46 -0400

    authenticating with firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdn7dTsVIKWTXH15xGOQtydjIm9FHUieE',
+  authDomain: 'tribe-app-db.firebaseapp.com',
+  projectId: 'tribe-app-db',
+  storageBucket: 'tribe-app-db.appspot.com',
+  messagingSenderId: '887487012508',
+  appId: '1:887487012508:web:45f3c6853a54b2b0115759',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);

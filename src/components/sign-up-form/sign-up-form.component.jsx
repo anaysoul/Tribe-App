ff92ce0214522891b08c2f68285fdb33fb0b1c0d
anaysoul <soulyana_lakew@outlook.com>
2022-03-20 21:10:13 -0400

    re-rendering from context

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
+import { UserContext } from '../../contexts/user.context';
 import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
@@ -18,7 +19,7 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
 
-  console.log(formFields);
+  console.log('HIT');
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);

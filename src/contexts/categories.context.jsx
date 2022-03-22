24c93476a2ffe97751b8ba92fac82a1437f4a204
anaysoul <soulyana_lakew@outlook.com>
2022-03-21 23:10:24 -0400

    using our CategoriesMap

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -4,12 +4,12 @@ import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 // import SHOP_DATA from '../shop-data.js';
 
-export const ProductsContext = createContext({
-  products: [],
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 });
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoiresMap] = useState({});
   // to fire off data to firestore to upload data
   // useEffect(() => {
   //   addCollectionAndDocuments('categories', SHOP_DATA);
@@ -20,14 +20,15 @@ export const ProductsProvider = ({ children }) => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
       console.log(categoryMap);
+      setCategoiresMap(categoryMap);
     };
     getCategoriesMap();
   }, []);
 
-  const value = { products, setProducts };
+  const value = { categoriesMap };
   return (
-    <ProductsContext.Provider value={value}>
+    <CategoriesContext.Provider value={value}>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   );
 };

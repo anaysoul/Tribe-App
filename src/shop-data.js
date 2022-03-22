08c265468b0f4e3b1f310e3a729b13edee936c69
anaysoul <soulyana_lakew@outlook.com>
2022-03-21 22:41:32 -0400

    addCollectionAndDocuments Pt2

--- src/shop-data.js
+++ src/shop-data.js
@@ -39,60 +39,29 @@ const SHOP_DATA = [
         name: 'Customized Cold Press Juice',
         imageUrl:
           'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/241474414_208142284680307_8469679974383104866_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=ENTsGq9o3HkAX8iwIlz&_nc_ht=scontent-lga3-1.xx&oh=00_AT_Q7KvQHF2pPpkRIt8rk2raFK4qA1zM7SU1IqHJJuPteQ&oe=623C29C6',
+        description: '',
         price: 10,
       },
     ],
   },
   {
-    title: 'All is well/ immunity shots',
+    title: 'All is Well Immunity Shots',
     items: [
       {
-        id: 10,
-        name: 'Adidas NMD',
-        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-        price: 220,
+        id: 6,
+        name: 'Woo-Tang',
+        imageUrl:
+          'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/271898836_289904879837380_5863436965268404964_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=1wrhYTiQZP8AX_akpZE&_nc_ht=scontent-lga3-1.xx&oh=00_AT9QUr12tjR62VhvSbqR0B7_Brnl0TgchS1b-m-zM5XvXw&oe=623DCFED',
+        description: 'Lemon, Ginger, Tumeric, Black Pepper, Cheyenne Pepper',
+        price: 5,
       },
       {
-        id: 11,
-        name: 'Adidas Yeezy',
-        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-        price: 280,
-      },
-      {
-        id: 12,
-        name: 'Black Converse',
-        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-        price: 110,
-      },
-      {
-        id: 13,
-        name: 'Nike White AirForce',
-        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-        price: 160,
-      },
-      {
-        id: 14,
-        name: 'Nike Red High Tops',
-        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
-        price: 160,
-      },
-      {
-        id: 15,
-        name: 'Nike Brown High Tops',
-        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
-        price: 160,
-      },
-      {
-        id: 16,
-        name: 'Air Jordan Limited',
-        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-        price: 190,
-      },
-      {
-        id: 17,
-        name: 'Timberlands',
-        imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-        price: 200,
+        id: 7,
+        name: 'Killa Beez',
+        imageUrl:
+          'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/274221116_310961991065002_6477990513099438169_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=a26aad&_nc_ohc=IdPO5y_sJCQAX80SLfg&_nc_ht=scontent-lga3-1.xx&oh=00_AT8UOHtsrz6_B5x2xrF10iJp4egASIJQzceA3_k9y7Yxjw&oe=623EECB9',
+        description: 'Lemon, Turmeric, Honey',
+        price: 5,
       },
     ],
   },
@@ -100,34 +69,52 @@ const SHOP_DATA = [
     title: 'Tribe Drinks 16oz',
     items: [
       {
-        id: 18,
-        name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-        price: 125,
+        id: 8,
+        name: 'Tribe Aid',
+        imageUrl:
+          'https://tastesbetterfromscratch.com/wp-content/uploads/2019/03/Strawberry-Lemonade-8-500x500.jpg',
+        description: 'Strawberry Lemonade',
+        price: 3.5,
       },
       {
-        id: 19,
-        name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-        price: 90,
+        id: 9,
+        name: 'Iced/Frozen Matcha',
+        imageUrl:
+          'https://www.ohhowcivilized.com/wp-content/uploads/2019/07/0719-matcha-frappuccino-7.jpg',
+        description: 'Strawberry Add-in Available',
+        price: 4,
       },
       {
-        id: 20,
-        name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-        price: 90,
+        id: 10,
+        name: 'MC Latte',
+        imageUrl:
+          'https://i2.wp.com/bakingmischief.com/wp-content/uploads/2021/02/iced-chai-tea-latte-image-683x1024.jpg',
+        description: 'Chai, Black Tea, Nutmeg, Vanilla Latte served over ice',
+        price: 4,
       },
       {
-        id: 21,
-        name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-        price: 165,
+        id: 11,
+        name: 'Public Lemony #1',
+        imageUrl:
+          'https://www.playpartyplan.com/wp-content/uploads/2021/09/creamy-lemonade-7-of-10.jpg',
+        description: 'Creamy Lemonade (Dairy)',
+        price: 4,
       },
       {
-        id: 22,
-        name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
-        price: 185,
+        id: 12,
+        name: 'Wild Berry Pomegranate Juice',
+        imageUrl:
+          'https://cdn.healthyrecipes101.com/wp-content/uploads/2020/10/Tips-for-Making-Pomegranate-Blueberry-Juice.jpg',
+        description: '',
+        price: 3.5,
+      },
+      {
+        id: 13,
+        name: 'Iced / Frozen Coffee',
+        imageUrl:
+          'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/274587741_312813510879850_2973076665596437501_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=a26aad&_nc_ohc=YhUl_uHyghAAX-yTXyU&_nc_ht=scontent-lga3-1.xx&oh=00_AT_gYjmq_dS-F5tywMeiDjvpXt3_uiN96BRsfhnniBI_kg&oe=623DFCF9',
+        description: '',
+        price: 4,
       },
     ],
   },
@@ -135,46 +122,30 @@ const SHOP_DATA = [
     title: 'Build Your Own Smoothies',
     items: [
       {
-        id: 23,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-        price: 25,
+        id: 14,
+        name: 'Fruit Options',
+        imageUrl:
+          'https://www.smalltownwoman.com/wp-content/uploads/2018/05/Summer-Fruit-Salad-3-III.jpg',
+        description:
+          'Mango, Pineapple, Strawberry, Banana, Blueberry, Mixed Berry',
+        price: 3,
       },
       {
-        id: 24,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        price: 20,
+        id: 15,
+        name: 'Base Options',
+        imageUrl:
+          'https://thenewsette.com/app/uploads/2018/04/shutterstock_356792147-milk_alternatives-blog-sm.jpg',
+        description: 'Whole Milk, Oat Milk, Almond Milk, Coconut Water',
+        price: 3,
       },
       {
-        id: 25,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 80,
-      },
-      {
-        id: 26,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 80,
-      },
-      {
-        id: 27,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-        price: 45,
-      },
-      {
-        id: 28,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-        price: 135,
-      },
-      {
-        id: 29,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-        price: 20,
+        id: 16,
+        name: 'Add-Ins',
+        imageUrl:
+          'https://www.joyfulhealthyeats.com/wp-content/uploads/2021/12/No-Bake-Chocolate-Peanut-Butter-Energy-Balls-web-1.jpg',
+        description:
+          'Almond Butter, Flaxseed, Chia seeds, hemp seed, Nutella, peanut butter, peanut butter powder, agave, honey, yogurt, protein powder, coconut flakes, Tribe Mix',
+        price: 0.5,
       },
     ],
   },
@@ -182,40 +153,75 @@ const SHOP_DATA = [
     title: 'Food Items',
     items: [
       {
-        id: 30,
-        name: 'Camo Down Vest',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-        price: 325,
+        id: 17,
+        name: 'Lil Tribe Bowl',
+        imageUrl:
+          'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/275125889_319123706915497_3518227365510990320_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=ybJy0Ja0FiEAX-aXxVe&_nc_ht=scontent-lga3-1.xx&oh=00_AT-Qg8F5UjkBfvR-7ufkSZn_lNVdv0Dl92Xz2CFr4hbgBg&oe=623EC981',
+        description:
+          'Blended Acai Berry with added blueberry, banana, almond butter, topped with strawberry, blueberry and banana',
+        price: 7,
       },
       {
-        id: 31,
-        name: 'Floral T-shirt',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-        price: 20,
+        id: 18,
+        name: 'Tribe Salad',
+        imageUrl:
+          'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/274482853_313425530818648_4633102519737583883_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=a26aad&_nc_ohc=7epqt9OqsSwAX_Ltsco&_nc_ht=scontent-lga3-1.xx&oh=00_AT9lOEd5t_07nGUGU_OVoHRwNRe_ZFw1mYlhO2pPLL35Nw&oe=623ED0CF',
+        description:
+          'spinach, cucumber, carrot, everything bagel seasoning, fresh strawberries and blueberries, mozzarella and feta cheese served with Parmesan crisp and your choice of a creamy Italian vinaigrette or Asian Thai dressing',
+        price: 7,
       },
       {
-        id: 32,
-        name: 'Black & White Longsleeve',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-        price: 25,
+        id: 19,
+        name: 'Panini',
+        imageUrl:
+          'https://i0.wp.com/chewnibblenosh.com/wp-content/uploads/2013/06/Grilled-Mozzarella-and-Spinach-BLTs-Chew-Nibble-Nosh.png?ssl=1',
+        description:
+          'Panini bread, spinach, mozzarella, tomato, Everything Bagel Seasoning',
+        price: 6,
       },
       {
-        id: 33,
-        name: 'Pink T-shirt',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        price: 25,
+        id: 20,
+        name: 'Avocado Toast',
+        imageUrl:
+          'https://valentinascorner.com/wp-content/uploads/2020/08/Avocado-Toast-3.jpg',
+        description:
+          'Toasted French Bread topped with Avocado spread and Everything Bagel Seasoning',
+        price: 5,
       },
       {
-        id: 34,
-        name: 'Jean Long Sleeve',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        price: 40,
+        id: 21,
+        name: 'Bagels',
+        imageUrl:
+          'https://cdn.shopify.com/s/files/1/0168/5018/products/Keto-Friendly-Bagels_900x.jpg?v=1600379424',
+        description:
+          'Plain, Everything Seasoned, Cinnamon Raisin | Spreads/Toppings - Honey, peanut butter, Nutella, Tribe mix, almond butter, flaxseed, Chia seeds, hemp seed, yogurt, coconut flakes',
+        price: 3,
       },
       {
-        id: 35,
-        name: 'Burgundy T-shirt',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-        price: 25,
+        id: 22,
+        name: 'Tribe Mix',
+        imageUrl:
+          'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/273302903_303069648520903_1454981720849930296_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=a26aad&_nc_ohc=mMkv7Gf0xwIAX8g3jc4&_nc_ht=scontent-lga3-1.xx&oh=00_AT9q1w-Cxs7piebjeFNovUdn7bTvstfS54dZCz4oxmDkSQ&oe=623DC91A',
+        description:
+          'dried cranberries, dried blueberries, pumpkin granola, flaxseed, hemp seed, Chia seed, sunflower seed kernels, peanuts',
+        price: 5,
+      },
+      {
+        id: 23,
+        name: "Nutty By Nature's",
+        imageUrl:
+          'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/272991396_298222609005607_816017839472929537_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=hFyh0MvJsZ0AX-9zIYU&_nc_ht=scontent-lga3-1.xx&oh=00_AT_DR-qtLsO9ix_CSGJk-rVHdU-Gta_BlnhIs4JdslnOpQ&oe=623CF96F',
+        description:
+          'oats, flaxseed, semi sweet chocolate chips, honey, peanut butter',
+        price: 5,
+      },
+      {
+        id: 24,
+        name: 'Mini Cheesecakes',
+        imageUrl:
+          'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_44/1502006/joy-bauer-today-main-191101-02.jpg',
+        description: '',
+        price: 3.5,
       },
     ],
   },

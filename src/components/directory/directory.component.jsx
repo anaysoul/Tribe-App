0797614c52ad59ac6df4fdc1a8e3c99a6bc9cf7c
anaysoul <soulyana_lakew@outlook.com>
2022-05-03 21:14:05 -0400

    updated category images

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,35 +6,35 @@ const categories = [
     id: 1,
     title: 'Cold Press Juice',
     imageUrl:
-      'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/272668265_295801445914390_6761783403408780219_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=5aH9mAqA1psAX-Ae3nh&tn=F-N0ZrL6P3BitgYl&_nc_ht=scontent-lga3-1.xx&oh=00_AT9ZQAbZoqWyEuXG4rCo3euvSdpAU7GuPWEvCD1IKFPwLQ&oe=623B8368',
+      'https://lifestylefrisco.com/wp-content/uploads/2021/05/cold-press-juice-scaled.jpg',
     route: 'shop/cold press juice',
   },
   {
     id: 2,
     title: 'All is Well Immunity Shots',
     imageUrl:
-      'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/276277714_327779976049870_1746200204362147358_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=USDZ0IuokTYAX8j27KF&_nc_ht=scontent-lga3-1.xx&oh=00_AT8yQiPBV7W0TyAMgibiKvR31vPjDkMTz5xRZsTVx0eYfA&oe=623A9BE7',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/juice-shot-variations-royalty-free-image-1576273623.jpg',
     route: 'shop/all is well immunity shots',
   },
   {
     id: 3,
     title: 'Tribe Drinks 16oz',
     imageUrl:
-      'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/275672424_324156473078887_7178719646634436105_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=NQjznVn_X0IAX_bDH1x&_nc_ht=scontent-lga3-1.xx&oh=00_AT_-JgbEfOYa7ZNnoiF-tecRgbGlZTVj6i7OveYsbnhZRA&oe=623BCC40',
+      'https://cdn.shopify.com/s/files/1/0531/8239/8633/products/VARIETY-PACK-FRUIT-DRINKS_400x.jpg?v=1616519354',
     route: 'shop/tribe drinks 16oz',
   },
   {
     id: 4,
     title: 'Build Your Own Smoothies',
     imageUrl:
-      'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/245509539_232081288953073_8038754401020890526_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=XNqsC1N3I6sAX8E9cvU&_nc_ht=scontent-lga3-1.xx&oh=00_AT8XbwVA8TjyzIqYSNqPpu6mxbkt2vJS8BDXHQQmPVe9Dg&oe=623C3F67',
+      'https://www.thelittleepicurean.com/wp-content/uploads/2017/01/green-smoothies.jpg',
     route: 'shop/build your own smoothies',
   },
   {
     id: 5,
     title: 'Food Items',
     imageUrl:
-      'https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/274987452_317165230444678_8246997711663751173_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=xF9c7zHaxTEAX9VNPES&_nc_ht=scontent-lga3-1.xx&oh=00_AT8VigJcN5_Aner17dMLAWXz_XrUiG5pyeKnqrp-eBtl9w&oe=623AA720',
+      'https://www.campuscooks.com/wp-content/uploads/2021/06/blog_snacks72.jpg',
     route: 'shop/food items',
   },
 ];
